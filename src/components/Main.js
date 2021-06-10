3c7a8a5befc1726b54af969c01ba81f283be4045
hamza ziouane <zioianhamza@gmail.com>
2021-06-11 00:52:28 +0100

    c1

--- src/components/Main.js
+++ src/components/Main.js
@@ -115,7 +115,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact</h2>
-          <form name="contact"  method="post" data-netlify="true" data-netlify-honepot ="bot-field"  action="#">
+          <form name="contact"  method="post" data-netlify="true" data-netlify-honepot ="bot-field"  >
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
