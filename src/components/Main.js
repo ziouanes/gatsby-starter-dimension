5694e06c2bbc52a097b71fd493359beaa6feaba8
hamza ziouane <zioianhamza@gmail.com>
2021-06-11 13:42:06 +0100

    dd

--- src/components/Main.js
+++ src/components/Main.js
@@ -124,7 +124,7 @@ class Main extends React.Component {
             </div>
             <div className="field half">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
+              <input type="email" name="email" id="email" />
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
